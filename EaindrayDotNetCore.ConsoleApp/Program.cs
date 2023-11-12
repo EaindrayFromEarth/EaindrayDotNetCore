5593909323efe912a3c6363044f2dd6554d3cd91
DESKTOP-DM20MV8\asus <eaindrayfromearth@gmail.com>
2023-11-12 08:09:01 -0800

    Add project files.

---
+++ EaindrayDotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EaindrayDotNetCore.ConsoleApp.AdoDotNetExamples;
+
+Console.WriteLine("Hello, World!");
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+
+adoDotNetExample.Run();
