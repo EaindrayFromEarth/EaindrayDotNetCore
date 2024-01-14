a8251d9cfc854aba265bd20e466842d617b83197
Eaindray <eaindrayfromearth@gmail.com>
2024-01-15 01:32:21 +0630

    "RestApiUrl": "https://localhost:7040", [Modify] JSON camelCase off

--- EaindrayDotNetCore.RestApi/Program.cs
+++ EaindrayDotNetCore.RestApi/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews().AddJsonOptions(opt =>
+{
+    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+    opt.JsonSerializerOptions.PropertyNameCaseInsensitive = false;
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EaindrayDotNetCore.RestApi.AppDbContext>(opt =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("DbConnection");
+    opt.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
