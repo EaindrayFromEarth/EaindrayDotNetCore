944fad91b5945ff8335c3837e202c9a270fb5ce0
Eaindray <eaindrayfromearth@gmail.com>
2024-01-15 01:24:14 +0630

    Refit.HttpClientFactory

--- EaindrayDotNetCore.MvcApp/Program.cs
+++ EaindrayDotNetCore.MvcApp/Program.cs
@@ -1,7 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Refit;
+using RestSharp;
+using EaindrayDotNetCore.MvcApp.EFDbContext;
+using EaindrayDotNetCore.MvcApp.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+},
+ServiceLifetime.Transient,
+ServiceLifetime.Transient);
+
+#region Refit
+
+builder.Services
+    .AddRefitClient<IBlogApi>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri(builder.Configuration.GetSection("RestApiUrl").Value!));
+
+#endregion
+
+#region HttpClient
+
+//builder.Services.AddScoped<HttpClient>();
+builder.Services.AddScoped(x => new HttpClient
+{
+    BaseAddress = new Uri(builder.Configuration.GetSection("RestApiUrl").Value!)
+});
+
+#endregion
+
+#region RestClient
+
+builder.Services.AddScoped(x => new RestClient(builder.Configuration.GetSection("RestApiUrl").Value!));
+
+#endregion
 
 var app = builder.Build();
 
