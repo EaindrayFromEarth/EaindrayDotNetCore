6f6b3f37cca50f09c5df370e1d9199afc2564707
Eaindray <eaindrayfromearth@gmail.com>
2024-01-15 01:04:54 +0630

    BlogRefitController

--- EaindrayDotNetCore.MvcApp/Controllers/BlogRefitController.cs
+++ EaindrayDotNetCore.MvcApp/Controllers/BlogRefitController.cs
@@ -1,4 +1,5 @@
 ﻿using EaindrayDotNetCore.MvcApp.Interfaces;
+using EaindrayDotNetCore.MvcApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EaindrayDotNetCore.MvcApp.Controllers
@@ -16,6 +17,42 @@ namespace EaindrayDotNetCore.MvcApp.Controllers
         {
             var model = await _blogApi.GetBlogs();
             return View(model);
+        }
+
+        [ActionName("Create")]
+        public IActionResult BlogRefitCreate()
+        {
+            return View("BlogRefitCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> BlogSave(BlogDataModel reqModel)
+        {
+            var model = await _blogApi.CreateBlog(reqModel);
+            return Redirect("/blogrefit");
+        }
+
+        [ActionName("Edit")]
+        public async Task<IActionResult> BlogRefitEdit(int id)
+        {
+            var model = await _blogApi.GetBlog(id);
+            return View("BlogRefitEdit", model);
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public async Task<IActionResult> BlogUpdate(int id, BlogDataModel reqModel)
+        {
+            var model = await _blogApi.UpdateBlog(id, reqModel);
+            return Redirect("/blogrefit");
+        }
+
+        [ActionName("Delete")]
+        public async Task<IActionResult> BlogDelete(int id)
+        {
+            var model = await _blogApi.DeleteBlog(id);
+            return Redirect("/blogrefit");
         }
     }
 }
