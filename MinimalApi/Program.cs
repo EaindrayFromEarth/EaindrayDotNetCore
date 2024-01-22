535c17db69e74b09454677302e74f3a7a48177c5
Eaindray <eaindrayfromearth@gmail.com>
2024-01-22 19:13:32 +0630

    AspNetCore Serilog

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,47 +1,62 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using MinimalApi;
-using MinimalApi.Features.Blog;
+using Serilog;
 using System.Text.Json.Serialization;
+using MinimalApi.Features.Blog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Hour, fileSizeLimitBytes: 1024)
+    .CreateLogger();
 
-//builder.Services.AddControllersWithViews().AddJsonOptions(opt =>
-//{
-//    opt.JsonSerializerOptions.PropertyNameCaseInsensitive = false;
-//    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-//});
-
-builder.Services.ConfigureHttpJsonOptions(option =>
+try
 {
-    option.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-    option.SerializerOptions.PropertyNamingPolicy = null;
-});
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
 
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Services.Configure<JsonOptions>(options =>
+    {
+        options.JsonSerializerOptions.PropertyNameCaseInsensitive = false;
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
 
-builder.Services.AddDbContext<AppDbContext>(opt =>
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-},
-ServiceLifetime.Transient,
-ServiceLifetime.Transient);
 
-var app = builder.Build();
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddDbContext<MinimalApi.AppDbContext>(opt =>
+    {
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+    },
+    ServiceLifetime.Transient,
+    ServiceLifetime.Transient);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.AddBlogService();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.AddBlogService();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
