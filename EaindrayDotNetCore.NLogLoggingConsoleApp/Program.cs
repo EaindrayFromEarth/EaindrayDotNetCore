b9da68c2dc5f0ae3b0af803cc04a0856cbffd9b7
Eaindray <eaindrayfromearth@gmail.com>
2024-01-24 19:53:03 +0630

    NLoggingConsole

---
+++ EaindrayDotNetCore.NLogLoggingConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+var logger = LoggerFactory.Create(builder => builder.AddNLog()).CreateLogger<Program>();
+logger.LogInformation("Program has started.");
+Console.ReadKey();
+
+Console.WriteLine("Hello, World!");
